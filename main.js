1fa6dd10121afcba1d4d86f62071b4285a25d24e
ActuallyAJL <zero3260@hotmail.com>
2022-03-29 15:28:44 -0500

    completed the basic madlib project

--- main.js
+++ main.js
@@ -5,46 +5,65 @@ console.log("You look marvelous!");
 
 const mainElement = document.querySelector("main");
 
-mainElement.addEventListener("click", event => {
-	if(event.target.id.startsWith("tellStory")){
-		const madlib = {
-			adjective:"",
-			sillyword:"",
-			pluralnoun:"",
-			noun:""
-		}
-		//set/save to sessionStorage
-		
-		//invoke renderStory
-	}
-})
+mainElement.addEventListener("click", (event) => {
+	event.preventDefault();
+  if (event.target.id.startsWith("tellStory")) {
+    const madlib = {
+      adjective: document.querySelector('#adj').value,
+      sillyword: document.querySelector('#silly').value,
+      pluralnoun: document.querySelector('#plNoun').value,
+      noun: document.querySelector('#noun').value,
+    };
+    //set/save to sessionStorage
+	setDataToStorage(madlib);
+    renderStory();
+  } else if (event.target.id.startsWith("startOver")) {
+	  renderInputs();
+  }
+});
 
 const getDataFromStorage = (dataKey) => {
-	//use JSON.parse()
-}
+  //use JSON.parse()
+  return JSON.parse(sessionStorage.getItem(dataKey));
+};
 
 const setDataToStorage = (dataObj) => {
-	//use JSON.stringify()
-}
+  //use JSON.stringify()
+  sessionStorage.setItem('myMadlib' , JSON.stringify(dataObj));
+};
 
 const clearStorage = (dataKey) => {
-	sessionStorage.removeItem(dataKey);
-}
+  sessionStorage.removeItem(dataKey);
+};
 
 const renderInputs = () => {
-	clearStorage('madlib')
-	// show inputs fields
+  clearStorage("madlib");
+  // show inputs fields
+  let inputHTML = `
+	<header><h1>Alex's Madlib Generator</h1></header>
+      <form id = 'inField''>
+        <input placeholder='Adjective' type="text" id="adj" name="adj" /><br /><br />
+        <input placeholder='Silly Word'type="text" id="silly" name="silly" /><br /><br />
+        <input placeholder='Plural Noun' type="text" id="plNoun" name="plNoun" /><br /><br />
+        <input placeholder='Noun'type="text" id="noun" name="noun" /><br /><br />
+        <button id="tellStory">Tell Me A Story</button>
+      </form>
 
-	//show 'Tell Story' button
-}
+      <div id="madlib-body"></div>
+	`;
+  mainElement.innerHTML = inputHTML;
+  //show 'Tell Story' button
+};
 
 const renderStory = () => {
-	//get from sessionStorage
-
-	//show the story
-
-	//show startOver button
-	//startOver will invoke renderInputs()
-}
+  //get from sessionStorage
+  const userSelection = getDataFromStorage('myMadlib');
+  //show the story
+  const story = `<h3>It had been a hard, ${userSelection.adjective} day on the ${userSelection.sillyword} trail. The cowboys drove a herd of ${userSelection.pluralnoun} across the dry plains, kicking up ${userSelection.noun} along the way as they looked for somewhere to bed down.</h3>`;
+  document.querySelector('#madlib-body').innerHTML = story;
+  //show startOver button
+  document.querySelector('#inField').innerHTML += `<button id="startOver">Start Over</button>`;
+  //startOver will invoke renderInputs()
+};
 
 renderInputs();
